ad0ce98589c9529ee5fc3fbd3bfd8f6e4c3855e0
Gabro057 <gabro057@seznam.cz>
2022-06-18 08:16:13 +0200

    Creating MongoDB and setting GraphQL

---
+++ readme.txt
@@ -0,0 +1,8 @@
+http://localhost:5000/graphql
+
+https://youtu.be/BcLNfwF04Kw
+
+npm run dev
+
+npm i express express-graphql graphql mongoose cors colors
+npm i -D nodemon dotenv
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const colors = require('colors')
+const cors = require('cors');
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const port = process.env.PORT || 5000
+const connectDB = require('./config/db')
+const schema = require('./schema/schema')
+
+const app = express()
+
+// Connect DB
+connectDB()
+
+app.use(cors());
+console.log("schema", schema)
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`))
